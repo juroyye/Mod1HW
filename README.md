8d071f75d8e212e0495f3b817bb7da5758ee2c6b
Justin M <manningjustin08@yahoo.com>
2023-06-29 17:43:29 -0400

    FOURTH COMMIT

--- README.md
+++ README.md
@@ -11,3 +11,162 @@ I am going to create well over the amount of boxes I need and create a function 
 
 CREATE DELETING/DISAPPEARING BOXES FUNCTION NEXT, TEST TO SEE IF IT WORKS.
 IF SO, CREATE MORE BOXES, ASSIGN VALUES, AND MAKE FINISHING TOUCHES.
+
+
+Option Tree:
+
+Accept Mission:
+
+   1. Asteroid field:
+
+    1a. Send an SOS signal and wait: (LOSS)
+
+         A scavenger robot spots you and alerts that you're hiding. You are surrounded by enemy forces and have no ther choice but to surrender.
+
+
+    1b. Enter a black hole
+
+         Wait for warp drive to recharge: (LOSS)
+
+         They pinged your location and surround your ship, forcing you to surrender.
+
+         Make communication with a nearby ship in hope that they will help you. (LOSS)
+
+         The ship is a pirate/pillaging vessel. They board your ship, tie you up, and steal the energy tesseract.
+
+//////////////////////////////////////////////////////////////////////////////////
+
+   2. Hypergate:
+    
+    2a. FLy towards the closest planet so their radars can't detect you:
+       
+           Land on their planet and request assistance: (LOSS)
+
+           As you enter the atmosphere, Scarr's military forces appear from the clouds. You try to flee, but they ultimately shoot you down, retrieving the tesseract in the process.
+
+//////////////////////////////////////////////////////////////////////////////////
+
+           - Go through next hypergate:
+
+            Reach out to the policing division and request protection as you return to home base.
+                 
+                   You gain protection, but Scarr's ships detect a massive envoy of ships moving on his radar. He pinpoints your position and they arrive through the hypergates and attack.
+
+                      Fight: (LOSS)
+
+                      Shot down in battle. 
+
+                      Run: 
+
+                      You try to flee and manage to escape through a hyper gate. 
+                      Upon leaving the gate and AFTER sending an SOS, your ship goes dead silent. Everything is dead, you're in pitch black space. 
+                      One of Scarr's ships follows you through the portal. About to destroy you and salvage the tesseract, you're faced with a decision.
+
+                        Surrender: (LOSS)
+
+                        They board ship, tie you up, take tesseract and leave through gate. Moments later, your forces of good arrive. You tell them you gave the tesseract up. The entire team breaks out into a frenzy of emotions. The universe is doomed.
+
+                        Refuse and face the consequences: (WIN)
+
+                        As the cannon of Lord Scarr's ship heats up and the bolt is nearly fired, a shimmer of light flashes through space. His ship instantly shuts off. A laser bolt that appears as lightning strikes through his ship, causing it to instantly explode in a fiery ball. Your forces have arrived. They board your ship and secure the tesseract from you. "Congratulations pilot, you have saved the universe."
+
+
+
+    2b. Send out an SOS signal and wait:
+
+
+     
+       
+ 
+
+
+
+
+ // if(option1.textContent.trim () === "") {
+//     option1.style.display = 'none';
+// }
+
+// if(option2.textContent.trim () === "") {
+//     option2.style.display = 'none';
+// }
+
+// if(option3.textContent.trim () === "") {
+//     option3.style.display = 'none';
+// }
+
+// if(option4.textContent.trim () === "") {
+//     option4.style.display = 'none';
+// }
+
+// if(option5.textContent.trim () === "") {
+//     option5.style.display = 'none';
+// }
+
+// if(option6.textContent.trim () === "") {
+//     option6.style.display = 'none';
+// }
+
+// if(option7.textContent.trim () === "") {
+//     option7.style.display = 'none';
+// }
+// if(option8.textContent.trim () === "") {
+//     option8.style.display = 'none';
+// }
+// if(option9.textContent.trim () === "") {
+//     option9.style.display = 'none';
+// }
+// if(option10.textContent.trim () === "") {
+//     option10.style.display = 'none';
+// }
+
+// if(option11.textContent.trim () === "") {
+//     option11.style.display = 'none';
+// }
+
+// if(option12.textContent.trim () === "") {
+//     option12.style.display = 'none';
+// }
+
+// if(option13.textContent.trim () === "") {
+//     option13.style.display = 'none';
+// }
+
+// if(option14.textContent.trim () === "") {
+//     option14.style.display = 'none';
+// }
+
+// if(option15.textContent.trim () === "") {
+//     option15.style.display = 'none';
+// }
+
+// if(option16.textContent.trim () === "") {
+//     option16.style.display = 'none';
+// }
+
+
+
+
+
+ //  document.getElementById("btn8").innerHTML = 
+        //  ``
+    
+        //  document.getElementById("btn9").innerHTML = 
+        //  ``
+
+        //  document.getElementById("btn10").innerHTML = 
+        //  ``
+  
+        //  document.getElementById("btn4").innerHTML = 
+        //  ``
+    
+        //  document.getElementById("btn5").innerHTML = 
+        //  ``
+  
+        //  document.getElementById("btn3").innerHTML = 
+        //  ``
+  
+        //  document.getElementById("btn2").innerHTML = 
+        //  ``
+        //  document.getElementById("btn7").innerHTML = 
+        //  ``
+   
--- script.js
+++ script.js
@@ -19,15 +19,49 @@ class LordScarr {
     }
 };
 
-// Functions List 
-
-
 
 // specifies the button and links it to js
+
+
+// option1 is accept mission and starts game
 let option1 = document.getElementById("btn1");
+
+// option2 goes into the hypergate option
 let option2 = document.getElementById("btn2");
+
+// option3 goes into the asteroid option
 let option3 = document.getElementById("btn3");
+
+// option4 allows you to enter the black hole
 let option4 = document.getElementById("btn4");
+
+// option5 goes into the first SOS option
+let option5 = document.getElementById("btn5");
+
+
+let option6 = document.getElementById("btn6");
+
+// option7 goes into the fly you TOWARDS the planet option
+let option7 = document.getElementById("btn7");
+
+// option8 goes into the second SOS option
+let option8 = document.getElementById("btn8");
+
+// option9 goes INTO the atmosphere and LAND option
+let option9 = document.getElementById("btn9");
+
+//option goes into the NEXT hypergate option
+let option10 = document.getElementById("btn10");
+
+
+let option11 = document.getElementById("btn11");
+let option12 = document.getElementById("btn12");
+let option13 = document.getElementById("btn13");
+let option14 = document.getElementById("btn14");
+let option15 = document.getElementById("btn15");
+let option16 = document.getElementById("btn16");
+let option17 = document.getElementById("resetBtn");
+
 
 
 // this is the beginning button that allows you to start 
@@ -38,6 +72,8 @@ function acceptMission() {
   option1.addEventListener('click', () => {       
     
     
+
+
     // alter the master text
      document.getElementById("masterText").innerHTML = 
 
@@ -50,25 +86,52 @@ function acceptMission() {
 
  
      // alter the buttons
-     document.getElementById("btn3").innerHTML = 
-     `Enter and hide in a nearby asteroid 
-     field until the enemy forces pass`
+
+        option1.style.display = 'none';
 
      document.getElementById("btn2").innerHTML = 
      `Attempt to fly across open space to a hypergate and 
      risk being captured`
-   
-     document.getElementById("btn1").innerHTML = 
-     ``
-   
+
+     document.getElementById("btn3").innerHTML =
+     `Attempt to hide in nearby asteroid field`
+
      
 });}
 acceptMission();
 
 
-// this button will allow you to select the asteroid option
+// this button will allow you to select the hypergate option
+  function viewHypergate() {     
+    
+    
+    option2.addEventListener('click', () => {       
+      
+      
+        
+      // alter the master text
+       document.getElementById("masterText").innerHTML = 
+  
+       `You pull back the throttle, accelerating amongst the
+       stars. Lord Scarr and his troops are firing laser 
+       bolts, barely grazing your wings. You make it into 
+       the hypergate and accelerate to the next solar system.`;
+  
+       option2.style.display = 'none';
+   
+       // alter the buttons
+       document.getElementById("btn7").innerHTML = 
+       `Fly towards the closest planet present`
+  
+       document.getElementById("btn8").innerHTML = 
+       `Send out an SOS to your team and wait`
+     
+       
+  });}
+ viewHypergate();
 
 
+ // this button will allow you to select the asteroid option
 function viewAst() {     
     
     
@@ -78,14 +141,16 @@ function viewAst() {
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `The asteroids`;
-  
-   
+       `The asteroids FIELD`;
+       
+       option2.style.display = 'none';
+       option3.style.display = 'none';
+
        // alter the buttons
-       document.getElementById("btn1").innerHTML = 
+       document.getElementById("btn4").innerHTML = 
        `Enter the black hole`
   
-       document.getElementById("btn2").innerHTML = 
+       document.getElementById("btn5").innerHTML = 
        `Send an SOS Signal and wait`
      
        
@@ -93,29 +158,106 @@ function viewAst() {
 viewAst();
   
 
-// this button will allow you to select the hypergate option
-  function viewHypergate() {     
+ // this function allows you to fly TOWARDS the planet
+ function flyToPlanet() { 
     
-    
-    option2.addEventListener('click', () => {       
+    option7.addEventListener('click', () => {       
       
       
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `You pull back the throttle, accelerating amongst the
-       stars. Lord Scarr and his troops are firing laser 
-       bolts, barely grazing your wings. You make it into 
-       the hypergate and accelerate to the next solar system.`;
+       `You spot a nearby planet in the distance,
+       YOU REALIZE ITS FRIENDLY
+       YOU ALSO SPOT ANOTHER HYPERGATE`;
   
+       option7.style.display = 'none';
    
        // alter the buttons
-       document.getElementById("btn1").innerHTML = 
-       `Fly towards the closest planet present`
+       document.getElementById("btn9").innerHTML = 
+       `FLY IN AND REQUEST ASSISTANCE`
   
-       document.getElementById("btn2").innerHTML = 
-       `Send out an SOS to your team and wait`
+       document.getElementById("btn10").innerHTML = 
+       `GO THROUGH NEXT HYPERGATE`
      
-       
-  });}
- viewHypergate();
+    });}
+    flyToPlanet();
+
+
+
+
+
+
+// this function enters planet: LOSS
+function enterPlanet(){
+    option9.addEventListener('click', () => {       
+      
+      
+        // alter the master text
+         document.getElementById("masterText").innerHTML = 
+    
+         `You pull into the atmosphere and Lord Scarr
+          shoots you down`;
+    
+          option9.style.display = 'none';
+     
+         // alter the buttons
+    
+        });}
+     enterPlanet();
+ 
+
+
+
+
+
+
+
+
+
+// this function enters the black hole
+
+function enterBlackHole() { 
+    
+    option4.addEventListener('click', () => {       
+      
+      
+      // alter the master text
+       document.getElementById("masterText").innerHTML = 
+  
+       `BLACCCCCC`;
+  
+       option4.style.display = 'none';
+   
+       // alter the buttons
+       document.getElementById("btn6").innerHTML = 
+       `Wait for warp drive to recharge`
+  
+       document.getElementById("btn11").innerHTML = 
+       `Make communication with nearby ship`
+     
+    });}
+    enterBlackHole();
+
+
+// this function sends an SOS Signal (FIRST ONE)
+
+function sendSos1() { 
+    
+    option5.addEventListener('click', () => {       
+      
+      
+      // alter the master text
+       document.getElementById("masterText").innerHTML = 
+  
+       `You send an sos signal, but a scavenger robot spots you 
+       and snitches. You surrender`;
+  
+        
+     
+    });}
+    sendSos1();
+
+// this function activates the reset button
+
+// hide button function
