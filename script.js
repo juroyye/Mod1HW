40c358a056562655d9d4de7b04f357b2445de107
Justin M <manningjustin08@yahoo.com>
2023-06-30 17:34:24 -0400

    DRAFT2 COMMIT

--- script.js
+++ script.js
@@ -77,22 +77,6 @@ let option17 = document.getElementById("btn17");
 let option18 = document.getElementById("resetBtn");
 
 
-// option2.style.display = 'none';
-// option3.style.display = 'none';
-// option4.style.display = 'none';
-// option5.style.display = 'none';
-// option6.style.display = 'none';
-// option7.style.display = 'none';
-// option8.style.display = 'none';
-// option9.style.display = 'none';
-// option10.style.display = 'none';
-// option11.style.display = 'none';
-// option12.style.display = 'none';
-// option13.style.display = 'none';
-// option14.style.display = 'none';
-// option15.style.display = 'none';
-// option16.style.display = 'none';
-// option17.style.display = 'none';
 
 if(100 == 100) {
 option2.style.display = 'none';
@@ -124,12 +108,19 @@ function acceptMission() {
     // alter the master text
      document.getElementById("masterText").innerHTML = 
 
-     `You manage to pull your spacecraft off of the launch 
-     platform and angle it towards the open gate, with 
+     `With the tesseract in the seat next to you, you manage
+      to pull your spacecraft off of the launch 
+     platform and angle it towards the open dock bay. Facing
      nothing but limitless stars and space consuming your 
-     front viewport. You hear Lord Scarr’s minions behind 
-     your ship, firing away at your fuselage, peppering the
-      underbelly. You are now faced with a critical decision:`;
+     front viewport and Lord Scarr’s minions behind 
+     your ship firing away at your fuselage, \r\n you jettison out 
+     of Lord Scarr's mothership "Scavenger" and enter into space.   
+     As you steer, you spot a hypergate in the distance that 
+     could supposedly bring you closer to Fusille. However, you 
+     can also see on your radar that emergency space fighters have
+     been deployed and are rapidly advancing. There's a nearby
+     asteroid field that might grant protection... \n Now having limited time, you are faced with a critical 
+     decision:`;
 
  
      // alter the buttons  
@@ -137,28 +128,10 @@ function acceptMission() {
 
 
      document.getElementById("btn2").innerHTML = 
-     `Attempt to fly across open space to a hypergate and 
-     risk being captured`
+     `ATTEMPT TO FLY ACROSS OPEN SPACE TO HYPERGATE`
 
      document.getElementById("btn3").innerHTML =
-     `Attempt to hide in nearby asteroid field`
-
-
-// option1.style.display = 'none';
-// option4.style.display = 'none';
-// option5.style.display = 'none';
-// option6.style.display = 'none';
-// option7.style.display = 'none';
-// option8.style.display = 'none';
-// option9.style.display = 'none';
-// option10.style.display = 'none';
-// option11.style.display = 'none';
-// option12.style.display = 'none';
-// option13.style.display = 'none';
-// option14.style.display = 'none';
-// option15.style.display = 'none';
-// option16.style.display = 'none';
-// option17.style.display = 'none';
+     `ATTEMPT TO HIDE IN NEARBY ASTEROID FIELD`
 
 option1.style.display = 'none';
 option2.style.display = 'initial';
@@ -174,37 +147,32 @@ acceptMission();
   function viewHypergate() {     
      option2.addEventListener('click', () => {       
       
-       
-
-        // option2.style.display = 'none';
-        // option3.style.display = 'none';
-        // option5.style.display = 'none';
-        // option6.style.display = 'none';
-        // option9.style.display = 'none';
-        // option10.style.display = 'none';
-        // option11.style.display = 'none';
-        // option12.style.display = 'none';
-        // option13.style.display = 'none';
-        // option14.style.display = 'none';
-        // option15.style.display = 'none';
-        // option16.style.display = 'none';
-        // option17.style.display = 'none';
         
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `You pull back the throttle, accelerating amongst the
-       stars. Lord Scarr and his troops are firing laser 
-       bolts, barely grazing your wings. You make it into 
-       the hypergate and accelerate to the next solar system.`;
+       `You figure that the asteroid field will only make 
+       you a sitting duck and an easy target. You pull back
+        the throttle, accelerating amongst the stars. Lord
+        Scarr and his troops continue in hot pursuit, firing laser 
+       bolts and cannons that narrowly graze your wings. You press the
+       throttle until you hear metal creaking; your palms are soaked 
+       in sweat. One of Scarr's goons races past you and then turns to unleash 
+    their rail gun. A perfectly executed barrel roll allows you to avoid
+       the situation, as the beam narrowly misses your cockpit.
+       Manuevering the control stick and grasping the throttle,
+       you glide cleanly into the hypergate and accelerate to the next 
+       solar system. Upon exiting the gate, you spot a purpleish planet, 
+       with storm circles forming on the surface. However, 
+       now might be a good time to call for help.`;
   
    
        // alter the buttons
        document.getElementById("btn7").innerHTML = 
-       `Fly towards the closest planet present`
+       `FLY TOWARDS THE PLANET IN SEEK OF HELP`
   
        document.getElementById("btn8").innerHTML = 
-       `Send out an SOS to your team and wait`
+       `SEND OUT AN SOS TO YOUR TEAM AND WAIT`
 
        option2.style.display = 'none';
        option3.style.display = 'none';
@@ -234,15 +202,23 @@ function viewAst() {
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `The asteroids FIELD`;
+       `Reaching the gate is impossible, you think to yourself, as there's 
+       no way you can out run some of the most merciless assassins in the universe.
+        You press the throttle even harder, rocketing toward the asteroid field.
+        You navigate around the first asteroid, dodge to the right of a second,
+        go under a third, even yet blast through a fourth. With each manuever, 
+        Scarr's henchmen fall further and further behind on your radar. You 
+        feel like you've lost them completely. You see a giant mass, which appears 
+        to be a black hole in the distance. No doubt, that could provide an escape.
+         However, you feel like calling for help would also be a great option right now...`;
        
 
        // alter the buttons
        document.getElementById("btn4").innerHTML = 
-       `Enter the black hole`
+       `ENTER THE BLACK HOLE`
   
        document.getElementById("btn5").innerHTML = 
-       `Send an SOS Signal and wait`
+       `SEND AN SOS SIGNAL AND WAIT`
      
 
        option4.style.display = 'initial';
@@ -267,9 +243,13 @@ viewAst();
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `You spot a nearby planet in the distance,
-       YOU REALIZE ITS FRIENDLY
-       YOU ALSO SPOT ANOTHER HYPERGATE`;
+       `Waiting around at the end of the hypergate is just as good as surrendering,
+       you reckon. They'll be following in no time. You take off towards the planet
+       intent on landing, requesting help from the natives, and hiding until the search 
+       party subdues. In the distance, however, you spot another hypergate that looks as if 
+       it was in disrepair. However, the rings are still functioning, and as such, could 
+       still transport you. If you go through, perhaps you can lose the fleet and not be followed
+       down to the planet.`;
   
        option7.style.display = 'none';
 
@@ -277,7 +257,7 @@ viewAst();
    
        // alter the buttons
        document.getElementById("btn9").innerHTML = 
-       `FLY IN AND REQUEST ASSISTANCE`
+       `LAND AND REQUEST ASSISTANCE`
   
        document.getElementById("btn10").innerHTML = 
        `GO THROUGH NEXT HYPERGATE`
@@ -301,8 +281,22 @@ function enterPlanet(){
         // alter the master text
          document.getElementById("masterText").innerHTML = 
     
-         `You pull into the atmosphere and Lord Scarr
-          shoots you down`;
+         `The hypergate looks too shoddy. You feel like it'd take immense luck
+          if you made it through, and even more luck if you could make it out alive.
+          You tear through open space and rocket down into the planet's atmosphere.
+          The ship rocks and creaks as it hits turbulence, bumping against pockets
+          of air. As you exit the clouds, the signs of civilization becomes visible.
+          Sprawling cities, farms, and roadways are strewn across the landscape. 
+          Although there are storm clouds in the distance, three seperate nearby stars 
+          shine brightly down on the terrain. In awe of your scenery, you are startled 
+          by a loud and definite warning coming from your radar. You look up, and 
+          quickly identify an emerging object from the clouds to be Scarr's fighter. 
+          You immediately pivot and begin to jettison away from him, however, you've 
+          already been spotted. Scarr doesn't hesitate to open fire. A thermal bolt
+        sizzles past you on your left, yet another on your right. A loud crack and boom
+        is felt from the back of your ship, and you realize you've been hit. Your craft 
+        spins uncontrollably, and as your cabin de-pressurizes, you fade out of 
+        consciousness.`;
     
           option9.style.display = 'none';
           option10.style.display = 'none';
@@ -329,7 +323,18 @@ function enterBlackHole() {
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `BLACCCCCC`;
+       `There's no point in waiting you think to yourself; that will only get 
+       you captured. You aim your vessel to the seething abyss of the hole, 
+       say a prayer, and press the throttle forward. The ship rumbles. 
+       The monitor glitches. The hole absorbs your ship and wraps your 
+       surroundings in darkness. You feel the fabric of your clothing start to bubble,
+       and your forehead begins to sweat profusely. You're pressed into the 
+       back of the seat as you reach speeds rarely comprehended or felt by man. Just as 
+       quickly as it began, it stops, and your ship is flung out into the Alden star system.
+       Your warp drive is fried, and the repair time is estimated by your console to be 2 hours.
+       However, there's a ship nearby that you can perhaps make contact with for help. 
+       You aren't familiar with this region, and as such, don't want to draw too much attention to 
+       yourself.`;
   
        option4.style.display = 'none';
 
@@ -337,10 +342,10 @@ function enterBlackHole() {
    
        // alter the buttons
        document.getElementById("btn6").innerHTML = 
-       `Wait for warp drive to recharge`
+       `WAIT FOR WARP DRIVE TO RECHARGE`
   
        document.getElementById("btn11").innerHTML = 
-       `Make communication with nearby pirate ship`
+       `MAKE COMMUNICATION WITH NEARBY SHIP`
      
 
        option6.style.display = 'initial';
@@ -366,8 +371,16 @@ function sendSos1() {
       // alter the master text
        document.getElementById("masterText").innerHTML = 
   
-       `You send an sos signal, but a scavenger robot spots you 
-       and snitches. You surrender`;
+       `You begin to imagine where the black hole can lead and quickly shake
+       off the thought. Who knows if this ship can handle such intense
+       speeds and temperatures, you think. Reaching out for help is your best option.
+       You press a few buttons on the console, and your location is immediately pinged
+       to your team some few star systems away. You rest your eyes for a second, 
+       able to take a deep breath at last. As you lean back in your seat, your ship lurches
+       right. You swiftly take up the controls, but it's too late. One of Scarr's minions
+       has you snagged in an energy net. You look frantically to see that your radar had been jammed.
+        You pull the throttle, but theres very little response. Asteroids crumble from right to left
+        as the enemy fleet closes in, pushing each rock out of the way. You have no choice but to surrender.`;
   
         // MAKE OPTIONS DISAPPEAR
 
@@ -393,7 +406,17 @@ function sendSos1() {
           // alter the master text
            document.getElementById("masterText").innerHTML = 
       
-           `They are pirates! You get executed.`
+           `You can't do this alone. Getting help is worth any potential risk.
+           After all, the universe is at stake and you believe in the goodness of 
+           others. You snatch the intercom, find the general radio channel, and 
+           speak as the mic crackles to life. Informing them that you need help to 
+           Fusille and that your warpdrive is drained, the ship rushes over to your 
+           position. Upon their approach, you spot an insignia roughly plastered to the 
+           side of the ship. Translated to English, it renders Ta'gari. You quickly
+           identify them as the merciless and cowardly pirating crew, responsible for 
+           the disappearance of thousands of spacecraft across dozens of galaxies.
+           Before you can react, their cannon jumps to life. You reach for the throttle, 
+           but it's too late; they fire.`
       
             // MAKE OPTIONS DISAPPEAR
          
@@ -418,8 +441,17 @@ function sendSos1() {
               // alter the master text
                document.getElementById("masterText").innerHTML = 
           
-               `As the warp drive charges, your location is pinged
-               and you get surrounded and captured.`
+               `You decide against asking for help. The ship could be full of bounty
+               hunters, or otherwise crooked individuals. You reroute power from
+               the lights and thrusters towards the recharging of the warp drive, 
+               and the wait time is cut to 30 minutes. As you stand up out of your seat,
+               you hear a faint beeping noise, seemingly coming from the underbelly of the ship.
+               You get on your hands and knees and quickly recognize the sound; a tracking device.
+               Frantically standing up and rushing back to your seat, you divert power back
+               to your thrusters, but it's too late. Roughly a dozen enemy ships warp in, clearly
+               sure of where you were located. As you begin to steer, an energy net is cast over
+               your vessel. Pressing the throttle provides little response. You have no choice 
+               but to surrender.`
           
                 // MAKE OPTIONS DISAPPEAR
 
